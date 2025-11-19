e752b42de58a1fd4a03542025cbc26634bb4afad
zakkmccrack <zaganelli.alessandro.wm@gmail.com>
2025-11-20 00:16:00 +0100

    Firs page created

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: 'https://zakkmccrack.github.io',
+    vite: {
+    plugins: [tailwindcss()],
+  },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        greenContrast: "var(--green-contrast)",
+      },
+    },
+  },
+};
