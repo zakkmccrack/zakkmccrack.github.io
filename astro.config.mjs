7dc9590b73d44b14316d0b969f43c0b0838e8087
zakkmccrack <zaganelli.alessandro.wm@gmail.com>
2025-12-07 23:55:34 +0100

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   integrations: [
     react(),
     icon(),
---
+++ src/data/tracks.js
@@ -0,0 +1,8 @@
+export const tracks = [
+  {
+    slug: "never_there",
+    title: "Never There",
+    band: "Sum 41",
+    file: "never_there.txt",
+  },
+];
